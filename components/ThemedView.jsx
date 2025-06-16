592f7cf347b68c00a9809d24e8a15a75bcd1f406
StevenMODIMO <stevepetes26@gmail.com>
2025-06-16 19:05:35 +0300

    react-native

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,12 +1,26 @@
-import { View, useColorScheme } from "react-native";
+import { SafeAreaView, View, useColorScheme } from "react-native";
 import { Colors } from "../constants/Colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function ThemedView({ style, ...props }) {
+export default function ThemedView({ style, safe = false, ...props }) {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
+  if (!safe)
+    return (
+      <View style={[style, { backgroundColor: theme.background }]} {...props} />
+    );
+
+  const insets = useSafeAreaInsets();
   return (
-    <View
-      style={[style, { backgroundColor: theme.background }]}
+    <SafeAreaView
+      style={[
+        style,
+        {
+          backgroundColor: theme.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        },
+      ]}
       {...props}
     />
   );
